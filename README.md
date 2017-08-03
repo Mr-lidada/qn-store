4ad6351919bab2baab5a6c5420123bd320753a03
ImCodeMonkey <liweioffices@163.com>
2017-08-03 14:32:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,90 @@
+最近对自己新开通的博客添加了七牛云存储的功能，网站上所有的图片等资源全部存放在七牛云。
+感谢[@Minwe LUO](https://github.com/minwe/qn-store)发表在github上的教程。
+
+### 说说自己的部署过程
+GHOST 1.3.0的目录结构和之前版本不是特别一样，我的博客是用GHOST-CLI部署的，所以大致的文件结构是这个样子：
+├── config.production.json
+├── content
+├── current
+├── system
+└── versions
+
+网站运行则依赖current文件夹下的内容，他下面的目录结构是这样的：
+├── content
+├── core
+├── Gruntfile.js
+├── index.js
+├── LICENSE
+├── MigratorConfig.js
+├── node_modules
+├── package.json
+├── PRIVACY.md
+├── README.md
+└── yarn.lock
+你会发现和以前的目录结构不太一样：
+├─buster
+├─content
+├─core
+├─node_modules
+├─config.example.js
+├─config.js
+├─Gruntfile.js
+├─index.js
+├─LICENSE
+├─npm-shrinkwrap.json
+├─package.json
+├─PRIVACY.md
+└─README.md
+config.js不见了，实际上config.js变成了/current/core/server/config下的defaults.json文件。所以配置"storage": {  ...}的话就要在defaults.json里面配置了。
+首先吧qn-store的插件放在/current/core/server/adapters/storage目录下然后在qn-store下执行npm install就可以了。
+```
+git clone https://github.com/ImCodeMonkey/qn-store.git
+cd qn-store
+npm install
+```
+最后在defaults.json加入下面的配置就可以了。
+```
+"storage": {
+    "active": "qn-store",
+    "qn-store": {
+      "accessKey": "xxxxxxxxx",
+      "secretKey": "xxxxxxxxx",
+      "bucket": "xxxxx",
+      "origin": "http://xxx.clouddn.com",
+      "uploadURL": "http://up-z2.qiniu.com",    //我是华南区，所以是这个地址了
+      "fileKey": {
+        "safeString": true,
+        "prefix": "YYYYMM/"
+      }
+    }
+},
+```
+### 更多设置
+```
+storage: {
+  active: 'qn-store',
+  'qn-store': {
+    accessKey: 'your access key',
+    secretKey: 'your secret key',
+    bucket: 'your bucket name',
+    origin: 'http://xx.xx.xx.glb.clouddn.com',
+    // timeout: 3600000, // default rpc timeout: one hour, optional
+    // if your app outside of China, please set `uploadURL` to `http://up.qiniug.com/`
+    // uploadURL: 'http://up.qiniu.com/'
+
+    // file storage key config [optional]
+    // if `fileKey` not set, Qiniu will use `SHA1` of file content as key.
+    fileKey: {
+      // use Qiniu hash as file basename, if set, `safeString` will be ignored
+      hashAsBasename: false,
+      safeString: true, // use Ghost safaString util to rename filename, e.g. Chinese to Pinyin
+      prefix: 'YYYY/MM/', // {String} will be formated by moment.js, using `[]` to escape,
+      suffix: '', // {String} string added before file extname.
+      extname: true // keep file's extname
+    }
+    // take `外面的世界 x.jpg` as example,
+    // applied above options, you will get an URL like below after uploaded:
+    //  http://xx.xx.xx.glb.clouddn.com/2016/06/wai-mian-de-shi-jie-x.jpg
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,196 @@
+/**
+ * Qiniu storage module for Ghost blog 1.x
+ * @see https://docs.ghost.org/v1.0.0/docs/using-a-custom-storage-module
+ */
+
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const urlParse = require('url').parse;
+const Promise = require('bluebird');
+const moment = require('moment');
+const qn = require('qn');
+const StorageBase = require('ghost-storage-base');
+
+const cwd = process.cwd();
+let ghostRoot;
+
+if (fs.existsSync(path.join(cwd, 'core'))) {
+  ghostRoot = cwd;
+} else if (fs.existsSync(path.join(cwd, 'current'))) {
+  // installed via ghost cli
+  ghostRoot = path.join(cwd, 'current');
+}
+
+if (!ghostRoot) {
+  throw new Error('Can not get ghost root path!');
+}
+
+const config = require(path.join(ghostRoot, 'core/server/config'));
+const utils = require(path.join(ghostRoot, 'core/server/utils'));
+const errors = require(path.join(ghostRoot, 'core/server/errors'));
+const i18n = require(path.join(ghostRoot, 'core/server/i18n'));
+const getHash = require('./lib/getHash');
+const logPrefix = '[QiniuStore]';
+
+class QiniuStore extends StorageBase {
+  constructor(options) {
+    super(options);
+
+    this.options = options || {};
+    this.client = qn.create(this.options);
+    this.storagePath = config.getContentPath('images');
+  }
+
+  /**
+   * Saves the image to storage
+   * - image is the express image object
+   * - returns a promise which ultimately returns the full url to the uploaded image
+   *
+   * @param file
+   * @param targetDir
+   * @returns {*}
+   */
+  save(file, targetDir) {
+    const client = this.client;
+    const _this = this;
+
+    return new Promise(function(resolve, reject) {
+      _this.getFileKey(file).then((function(key) {
+        client.upload(fs.createReadStream(file.path), {
+          key: key
+        }, function(err, result) {
+          console.log(logPrefix, result);
+          // console.log('[' + err.code + '] ' + err.name);
+          err ? reject(err) : resolve(result.url);
+        });
+      }));
+    });
+  }
+
+  /**
+   * don't need it in Qiniu
+   * @param filename
+   * @param targetDir
+   * @returns {*|bluebird}
+   * @see https://support.qiniu.com/hc/kb/article/112817/
+   * TODO: if fileKey option set, should use key to check file whether exists
+   */
+  exists(filename, targetDir) {
+    return new Promise(function(resolve, reject) {
+      resolve(false);
+    });
+  }
+
+  /*
+  exists(filename) {
+    return new Promise(function(resolve, reject) {
+      // send key to get image info
+      client.stat(filename, function(err, info) {
+        if (info) {
+          resolve(true);
+        } else if (err && err.code === 612) { // File not exists
+          resolve(false);
+        } else {
+          reject('Can\'t get file info.');
+        }
+      });
+    });
+  }*/
+
+  // middleware for serving the files
+  serve() {
+    // a no-op, these are absolute URLs
+    return function(req, res, next) {
+      next();
+    };
+  }
+
+  /**
+   * Not implemented.
+   * @description not really delete from Qiniu, may be implemented later
+   * @param fileName
+   * @param targetDir
+   * @returns {*|bluebird}
+   */
+  delete(fileName, targetDir) {
+    // return Promise.reject('not implemented');
+    return new Promise(function(resolve, reject) {
+      resolve(true);
+    });
+  }
+
+  /**
+   * Reads bytes from Qiniu for a target image
+   *
+   * @param options
+   */
+  read(options) {
+    options = options || {};
+
+    const client = this.client;
+    const key = urlParse(options.path).pathname.slice(1);
+
+    return new Promise(function(resolve, reject) {
+      client.download(key, function(err, content, res) {
+        if (err) {
+          return reject(new errors.GhostError({
+            err: err,
+            message: `${logPrefix} Could not read image: ${options.path}`,
+          }));
+        }
+
+        resolve(content);
+      });
+    });
+  }
+
+  getFileKey(file) {
+    const keyOptions = this.options.fileKey;
+    let fileKey = null;
+
+    if (keyOptions) {
+      const getValue = function(obj) {
+        return typeof obj === 'function' ? obj() : obj;
+      };
+      const ext = path.extname(file.name);
+      let basename = path.basename(file.name, ext);
+      let prefix = '';
+      let suffix = '';
+      let extname = '';
+
+      if (keyOptions.prefix) {
+        prefix = moment().format(getValue(keyOptions.prefix))
+          .replace(/^\//, '');
+      }
+
+      if (keyOptions.suffix) {
+        suffix = getValue(keyOptions.suffix);
+      }
+
+      if (keyOptions.extname !== false) {
+        extname = ext.toLowerCase();
+      }
+
+      const contactKey = function(name) {
+        return prefix + name + suffix + extname;
+      };
+
+      if (keyOptions.hashAsBasename) {
+        return getHash(file).then(function(hash) {
+          return contactKey(hash);
+        });
+      } else if (keyOptions.safeString) {
+        basename = utils.safeString(basename);
+      }
+
+      fileKey = contactKey(basename);
+    }
+
+    return Promise.resolve(fileKey);
+  }
+}
+
+module.exports = QiniuStore;
